c779e4944422ed0407e7a60d137380b584f632c7
bharath.mannaperumal <bharath.mannaperumal@CDC2-D-9R5J882.dir.svc.accenture.com>
2017-06-12 13:08:55 +0530

    zipkin server added

--- monitoring/src/main/java/com/accenture/MonitoringApplication.java
+++ monitoring/src/main/java/com/accenture/MonitoringApplication.java
@@ -3,11 +3,13 @@ package com.accenture;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+import zipkin.server.EnableZipkinServer;
+
 
 @SpringBootApplication
 @EnableHystrixDashboard
-@EnableZuulServer
+@EnableZipkinServer
 public class MonitoringApplication {
 
 	public static void main(String[] args) {
