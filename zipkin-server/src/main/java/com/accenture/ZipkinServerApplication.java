858222b9e5caeddeb8ff854b6bd25633d76af37f
bharath <bharathkumar.feb14@gmail.com>
2017-06-12 22:29:18 +0530

    zipkin server added

--- zipkin-server/src/main/java/com/accenture/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/com/accenture/ZipkinServerApplication.java
@@ -2,14 +2,15 @@ package com.accenture;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+import zipkin.server.EnableZipkinServer;
 
 
 @SpringBootApplication
-@EnableHystrixDashboard
-public class MonitoringApplication {
+@EnableZipkinServer
+public class ZipkinServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MonitoringApplication.class, args);
+		SpringApplication.run(ZipkinServerApplication.class, args);
 	}
 }
