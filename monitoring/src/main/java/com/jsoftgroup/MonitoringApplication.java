f565e443817afb116001a727bb377cd136d206e1
bharath <bharathkumar.feb14@gmail.com>
2018-08-11 18:59:42 +0530

    Spring boot admin enabled

--- monitoring/src/main/java/com/jsoftgroup/MonitoringApplication.java
+++ monitoring/src/main/java/com/jsoftgroup/MonitoringApplication.java
@@ -2,11 +2,16 @@ package com.jsoftgroup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 
 @SpringBootApplication
 @EnableHystrixDashboard
+@EnableDiscoveryClient
+@EnableAdminServer
 public class MonitoringApplication {
 	
 	public static void main(String[] args) {
