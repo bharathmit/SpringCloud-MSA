b3347444f6aa86a259e3e317b414fa22493f78ce
Bharath Mannaperumal <bmannape@ford.com>
2021-11-29 13:09:49 +0530

    gateway issue fixed

--- gateway/src/main/java/com/jsoftgroup/GatewayApplication.java
+++ gateway/src/main/java/com/jsoftgroup/GatewayApplication.java
@@ -3,12 +3,12 @@ package com.jsoftgroup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 /*import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger.web.UiConfiguration;*/
 
+@EnableEurekaClient
 @SpringBootApplication
-@EnableDiscoveryClient
-
 public class GatewayApplication {
 	
 	public static void main(String[] args) {
