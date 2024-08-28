545008eaa9b6b53742b6482f3b7e1d972bb6fcaf
Bharath Mannaperumal <D0106527@ncsdevnet.com>
2024-08-28 11:00:58 +0800

    Spring boot 3 version upgrade

--- inventory/src/main/java/com/jsoftgroup/repo/InventoryJPARepo.java
+++ inventory/src/main/java/com/jsoftgroup/repo/InventoryJPARepo.java
@@ -2,7 +2,7 @@ package com.jsoftgroup.repo;
 
 
 
-import javax.transaction.Transactional;
+import jakarta.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
